45f7176fa70c2b3e73dd37662b9f7cef2ff3c9a0
jiazou-bigdata <jacquelinezou@gmail.com>
2019-11-23 19:55:19 +0000

    add comments

--- benchmark/src/headers/STLSlabAllocator.h
+++ benchmark/src/headers/STLSlabAllocator.h
@@ -11,6 +11,10 @@
 #include "SlabAllocator.h"
 #include <iostream>
 
+/*
+ * This class wraps a Memcached SlabAllocator with STL-compatible interfaces.
+ */
+
 template <class T>
 class STLSlabAllocator {
 public:
@@ -22,26 +26,35 @@ public:
     typedef T& reference;
     typedef const T& const_reference;
 
+    //constructor that allocates a memory pool of the given size from the heap
+    //@param size, the size of the memory pool
     STLSlabAllocator(size_t size) {
         allocator = make_shared<SlabAllocator>(size, true);
     }
+
+    //constructor that uses a memory pool passed in
+    //@param memPool, the pointer to the memory pool passed in
+    //@param size, the size of the memory pool passed in
     STLSlabAllocator(void* memPool, size_t size) {
         allocator = make_shared<SlabAllocator>(memPool, size, true);
     }
 
+    //copy constructor
     template <class U>
     STLSlabAllocator(const STLSlabAllocator<U>& other) {
         allocator = other.getAllocator();
     }
+
+    
     template <class U>
     struct rebind {
         typedef STLSlabAllocator<U> other;
     };
+
+    //to allocate a piece of memory of given size
+    //@param n, size of memory to allocate
     pointer allocate(size_type n, const void* /*hint*/ = 0) {
         void* retPtr = allocator->do_slabs_alloc(n * sizeof(T));
-        // if(n*sizeof(T)!=24) {
-        // cout << "Size to allocate:"<< n*sizeof(T)<<"\n";
-        //}
         if (retPtr == nullptr) {
             std::bad_alloc exception;
             cout << "Can't allocate more from the page! TODO: add a new page!\n";
@@ -49,10 +62,15 @@ public:
         }
         return (pointer)retPtr;
     }
+
+    //to deallocate a piece of memory of given size
+    //@param p, pointer to the memory to deallocate
+    //@param n, size of memory to deallocate
     void deallocate(pointer p, size_type n) {
-        // cout << "Size to deallocate:"<< n*sizeof(T)<<"\n";
         return allocator->do_slabs_free(p, n * sizeof(T));
     }
+
+
     void construct(pointer p, const T& val) {
         new (p) T(val);
     }
